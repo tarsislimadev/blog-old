a3fc3a622cb5806c368210a3abd61d7705fdb528
Tarsis de Lima <br.tmvdl@gmail.com>
2022-07-07 01:46:11 -0300

    Init

---
+++ env/dev.sh
@@ -0,0 +1,3 @@
+docker run -it --user node -v $(pwd):/app -w /app --network host node:lts-alpine npm run dev
+
+
---
+++ env/run.sh
@@ -0,0 +1,1 @@
+docker run -it --user node -v $(pwd):/app -w /app --network host node:lts-alpine sh 
