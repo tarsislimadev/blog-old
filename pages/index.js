c66dc618120cfad167fdc4fcf1d67c194c6c797a
Tarsis de Lima <br.tmvdl@gmail.com>
2022-07-07 02:05:49 -0300

    Blank space

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      Hello World. 
+      Hello World.
       <Link href="/about">About</Link>
     </div>
   )
