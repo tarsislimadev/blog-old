fcaccd08dff7f50e16caf09e01c4b96fcd95b1d2
Tarsis de Lima <br.tmvdl@gmail.com>
2022-07-07 22:10:18 -0300

    Update environment files

--- env/dev.sh
+++
@@ -1,3 +0,0 @@
-docker run -it --user node -v $(pwd):/app -w /app --network host node:lts-alpine npm run dev
-
-
--- env/run.sh
+++ env/run.sh
@@ -1,1 +1,3 @@
-docker run -it --user node -v $(pwd):/app -w /app --network host node:lts-alpine sh 
+docker run -it --user node -v $(pwd):/app -w /app --network host node:lts-alpine npm run dev
+
+
